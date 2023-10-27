8f93bfd1417f83b8daafdc0df5399fe8ee3a780e
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-10-27 23:09:47 +0600

    slider done

--- app/(home)/_components/Slider.tsx
+++ app/(home)/_components/Slider.tsx
@@ -1,7 +1,11 @@
 'use client';
 
+import { buttonVariants } from '@/components/ui/Button';
+import Overlay from '@/components/ui/Overlay';
 import { data } from '@/data/sliderContents';
+import { cn } from '@/lib/utils';
 import Image from 'next/image';
+import Link from 'next/link';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -10,7 +14,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Slider = () => {
   return (
-    <section className='h-[calc(100vh-5rem)]'>
+    <section className='h-[calc(100vh-5rem)] w-full'>
       <Swiper
         pagination={{ clickable: true }}
         navigation={true}
@@ -25,14 +29,25 @@ const Slider = () => {
         className='mySwiper h-full w-full'
       >
         {data.map((slide) => (
-          <SwiperSlide key={slide.heading} className='h-full w-full'>
+          <SwiperSlide key={slide.heading} className='relative h-full w-full'>
             <Image
               src={slide.image}
               alt={slide.heading}
-              width={1125}
-              height={750}
+              width={1920}
+              height={1080}
               className='h-full w-full object-cover'
             />
+            <Overlay />
+            <div className='sp container absolute bottom-0 left-0 right-0 top-0 h-full w-full space-y-5 text-white'>
+              <h1>{slide.heading}</h1>
+              <p className='max-w-6xl'>{slide.subHeading}</p>
+              <Link
+                href='/beauty-packages'
+                className={cn(buttonVariants({ variant: 'secondary' }))}
+              >
+                Browse Beauty Packages
+              </Link>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
